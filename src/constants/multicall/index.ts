c0f943767850c29374a2c6440e8538becc2b5ccb
828q <orbiiverde@gmail.com>
2026-08-27 00:18:05 +0000

    Initial commit

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -2,7 +2,7 @@ import { ChainId } from '@raptordex/sdk'
 import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '0xFfdE59FCbe1AE8B2a5015C24d954a3C1bD14DFA4'
+  [ChainId.MAINNET]: '0x68971ffaaBbf9b78CE08D6880368604a9CcB8F95'
 }
 
 export { MULTICALL_ABI, MULTICALL_NETWORKS }
