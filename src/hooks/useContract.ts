bb7dc5b478ba68e328acd9e546c7f5fc22eba9b8
828q <orbiiverde@gmail.com>
2026-08-27 00:29:52 +0000

    fix

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -1,5 +1,5 @@
 import { Contract } from '@ethersproject/contracts'
-import { ChainId, WETH } from '@raptordex/sdk'
+import { ChainId } from '@raptordex/sdk'
 import { abi as IUniswapV2PairABI } from '@uniswap/v2-core/build/IUniswapV2Pair.json'
 import { useMemo } from 'react'
 import { ERC20_BYTES32_ABI } from '../constants/abis/erc20'
@@ -45,8 +45,11 @@ export function useTokenContract(tokenAddress?: string, withSignerIfPossible?: b
 }
 
 export function useWETHContract(withSignerIfPossible?: boolean): Contract | null {
-  const { chainId } = useActiveWeb3React()
-  return useContract(chainId ? WETH[chainId].address : undefined, WETH_ABI, withSignerIfPossible)
+  return useContract(
+    '0xeF7cADE66695f4cD8a535f7916fBF659936818C4',
+    WETH_ABI,
+    withSignerIfPossible
+  )
 }
 
 export function useBytes32TokenContract(tokenAddress?: string, withSignerIfPossible?: boolean): Contract | null {
